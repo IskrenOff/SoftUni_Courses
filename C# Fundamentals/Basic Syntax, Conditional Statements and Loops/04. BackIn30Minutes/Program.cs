38b40d1270283547b897bb4ee7f4ae90fd76033c
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-05-25 20:31:55 +0300

    add

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops/04. BackIn30Minutes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _04._BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int houer = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes > 59)
+            {
+                minutes -= 60;
+                houer += 1;
+            }
+            if (houer > 23)
+            {
+                houer = 0;
+            }
+            Console.WriteLine($"{houer}:{minutes:D2}");
+
+
+        }
+    }
+}
