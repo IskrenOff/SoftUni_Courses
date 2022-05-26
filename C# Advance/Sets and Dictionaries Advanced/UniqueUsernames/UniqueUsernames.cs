fddf728e99b1241ab7fc8eda1cd03fcec718f705
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-05-26 07:56:50 +0300

    add

---
+++ C# Advance/Sets and Dictionaries Advanced/UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersOfUsernames = int.Parse(Console.ReadLine());
+
+            HashSet <string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < numbersOfUsernames; i++)
+            {
+                usernames.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine,usernames));
+        }
+    }
+}
