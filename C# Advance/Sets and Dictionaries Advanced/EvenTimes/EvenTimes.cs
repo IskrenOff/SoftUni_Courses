206735b1590f8facb6a964738630439394f1ed96
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-05-26 08:45:08 +0300

    add new solved problem

---
+++ C# Advance/Sets and Dictionaries Advanced/EvenTimes/EvenTimes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    internal class EvenTimes
+    {
+        static void Main(string[] args)
+        {
+            int cntOfIntegers = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> integerCollection = new Dictionary<int, int>();
+
+            for (int i = 0; i < cntOfIntegers; i++)
+            {
+                int integer = int.Parse(Console.ReadLine());
+
+                if (!integerCollection.ContainsKey(integer))
+                {
+                    integerCollection.Add(integer, 1);
+                }
+                else
+                {
+                    integerCollection[integer]++;
+                }
+            }
+
+            foreach (var item in integerCollection)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
