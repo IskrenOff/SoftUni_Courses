34efba686f531628daf89f95c7573d361662029e
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-05-26 08:58:37 +0300

    solved problem

---
+++ C# Advance/Sets and Dictionaries Advanced/CountSymbols/CountSymbols.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    internal class CountSymbols
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine()
+                .ToCharArray();
+
+            SortedDictionary<char, int> dictionaryOfSymbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!dictionaryOfSymbols.ContainsKey(text[i]))
+                {
+                    dictionaryOfSymbols.Add(text[i], 1);
+                }
+                else
+                {
+                    dictionaryOfSymbols[text[i]]++;
+                }
+            }
+
+            foreach (var symbol in dictionaryOfSymbols)
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
