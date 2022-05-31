4e3ce3ebd92486bdd43817adb7377a4df7e25ae7
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-05-31 18:35:28 +0300

    anather solved problems

---
+++ C# Advance/Functional Programming/CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    internal class CustomMinFunction
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            Func<int , int> smallestNumber = number => input.Min();
+            Console.WriteLine(smallestNumber);
+        }
+    }
+}
