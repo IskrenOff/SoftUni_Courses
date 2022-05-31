8332fc369114bbdd7ad7db115074539a61c16b9e
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-05-31 19:48:04 +0300

    new

--- C# Advance/Functional Programming/FindEvensOrOdds/FindEvensOrOdds.cs
+++ C# Advance/Functional Programming/FindEvensOrOdds/FindEvensOrOdds.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FindEvensOrOdds
 {
-    internal class Program
+    internal class FindEvensOrOdds
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            int startNumber = int.Parse(input.Split(' ')[0]);
+            int endNumber = int.Parse(input.Split(' ')[1]);
+
+            List<int> numbers = new List<int>();
+
+            for (int number = startNumber; number <= endNumber; number++)
+            {
+                numbers.Add(number);
+            }
+
+            string type = Console.ReadLine();
+
+            Predicate<int> predicate = null;
+
+            switch (type)
+            {
+                case "even":
+                    predicate = number => number % 2 == 0;
+                    break;
+                case "odd":
+                    predicate = number => number % 2 != 0;
+                    break;
+            }
+
+            Console.WriteLine(string.Join(" ", numbers.FindAll(predicate)));
         }
     }
 }
