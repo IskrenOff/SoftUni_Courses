a46e033a81b5b9af5d0e38f86634d96d60d4a8c5
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-06-02 19:03:03 +0300

    solved

--- C# Advance/Defining Classes/DefineAClassPerson/Person.cs
+++ C# Advance/Defining Classes/DefineAClassPerson/Person.cs
@@ -9,6 +9,16 @@ namespace DefiningClasses
         private string name;
         private int age;
 
+        public Person ()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+        public Person(int age)
+        {
+            Name = "No name";
+            Age=age;
+        }
         public Person(string name, int age)
         {
             Name = name;
