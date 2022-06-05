297e174bcf1fc8daa660a311afa3a120ea244803
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-06-05 09:55:45 +0300

    next

--- C# Advance/Exam Preparation/MealPlan/MealPlan.cs
+++ C# Advance/Exam Preparation/MealPlan/MealPlan.cs
@@ -13,15 +13,12 @@ namespace MealPlan
                 .Split(' ')
                 .Select(int.Parse)
                 .ToArray();
-
-            Dictionary<string, int> mealsData = new Dictionary<string, int>
-            {
-                {"salad" , 350},
-                {"soup" , 490},
-                {"pasta" , 680},
-                {"steak" , 790},
-            };
-
+            // Calories per meal
+            int salad = 350;
+            int soup = 490;
+            int pasta = 680;
+            int steak = 790;
+            // Meals counter
             int mealsCnt = 0;
 
             // Fill the Stack with calories intake per day
@@ -33,46 +30,128 @@ namespace MealPlan
                 {
                     case "salad":
                         int currentCalories = caloriesStackPerDay.Pop();
-                        currentCalories -= mealsData[meals[i]];
-                        
-                        if (currentCalories < mealsData[meals[i]])
+                        if (currentCalories >= salad)
                         {
-                            currentCalories = mealsData[meals[i]] - currentCalories;
-                        }
-                        else if (currentCalories <= 0)
-                        {
-                            mealsData[meals[i]] -= currentCalories;
+                            currentCalories -= salad;
+                            mealsCnt ++;
+                            meals.Remove(meals[i]);
+                            i--;
 
-                            if (mealsData[meals[i]] <= 0)
+                            if (currentCalories > 0)
                             {
-                                mealsData.Remove(meals[i]);
+                                caloriesStackPerDay.Push(currentCalories);
                             }
                         }
                         else
                         {
-                            caloriesStackPerDay.Push(currentCalories);
+                            mealsCnt ++;
+                            salad -= currentCalories;
+                            if (caloriesStackPerDay.Count > 0)
+                            {
+                                currentCalories = caloriesStackPerDay.Pop() - salad;
+                                caloriesStackPerDay.Push(currentCalories);
+                            }
+                            meals.Remove(meals[i]);
+                            i--;
                         }
-                        break;
-                    case "soup":
-                        JohnCalories(mealsData,caloriesStackPerDay,meals);
-                        break;
-                    case "pasta":
-                        break;
-                    case "steak":
                         
                         break;
+                    case "soup":
+                        int currentCalories2 = caloriesStackPerDay.Pop();
+                        if (currentCalories2 >= soup)
+                        {
+                            currentCalories2 -= soup;
+                            mealsCnt++;
+                            meals.Remove(meals[i]);
+                            i--;
+
+                            if (currentCalories2 > 0)
+                            {
+                                caloriesStackPerDay.Push(currentCalories2);
+                            }
+                        }
+                        else
+                        {
+                            mealsCnt++;
+                            soup -= currentCalories2;
+                            if (caloriesStackPerDay.Count > 0)
+                            {
+                                currentCalories = caloriesStackPerDay.Pop() - soup;
+                                caloriesStackPerDay.Push(currentCalories);
+                            }
+                            meals.Remove(meals[i]);
+                            i--;
+                        }
+                        break;
+                    case "pasta":
+                        int currentCalories3 = caloriesStackPerDay.Pop();
+                        if (currentCalories3 >= pasta)
+                        {
+                            currentCalories3 -= pasta;
+                            mealsCnt++;
+                            meals.Remove(meals[i]);
+                            i--;
+
+                            if (currentCalories3 > 0)
+                            {
+                                caloriesStackPerDay.Push(currentCalories3);
+                            }
+                        }
+                        else
+                        {
+                            mealsCnt++;
+                            pasta -= currentCalories3;
+                            if (caloriesStackPerDay.Count > 0)
+                            {
+                                currentCalories = caloriesStackPerDay.Pop() - pasta;
+                                caloriesStackPerDay.Push(currentCalories);
+                            }
+                            meals.Remove(meals[i]);
+                            i--;
+                        }
+                        break;
+                    case "steak":
+                        int currentCalories4 = caloriesStackPerDay.Pop();
+                        if (currentCalories4 >= steak)
+                        {
+                            currentCalories4 -= steak;
+                            mealsCnt++;
+                            meals.Remove(meals[i]);
+                            i--;
+
+                            if (currentCalories4 > 0)
+                            {
+                                caloriesStackPerDay.Push(currentCalories4);
+                            }
+                        }
+                        else
+                        {
+                            mealsCnt++;
+                            steak -= currentCalories4;
+                            if (caloriesStackPerDay.Count > 0)
+                            {
+                                currentCalories = caloriesStackPerDay.Pop() - steak;
+                                caloriesStackPerDay.Push(currentCalories);
+                            }
+                            meals.Remove(meals[i]);
+                            i--;
+                        }
+                        break;
+                }
+                if (meals.Count == 0 || caloriesStackPerDay.Count == 0)
+                {
+                    break;
                 }
             }
-        }
 
-        public static void JohnCalories(Dictionary<string, int> mealsData, Stack<int> caloriesStackPerDay, List<string> meals)
-        {
-            for (int i = 0; i < meals.Count; i++)
+            if (caloriesStackPerDay.Count == 0)
             {
-                int currentCalories = caloriesStackPerDay.Pop();
-
-
+                Console.WriteLine($"John ate enough, he had {mealsCnt} meals.\nMeals left: {string.Join(", ",meals)}.");
             }
-        }
+            else
+            {
+                Console.WriteLine($"John had {mealsCnt} meals.\nFor the next few days, he can eat {string.Join(", ",caloriesStackPerDay)} calories.");
+            }
+        }      
     }
 }
