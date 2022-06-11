093e7f3012d21949688a91a3e34b79bde48ad7ae
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-06-11 13:43:27 +0300

    da

--- C# Advance/Exam Preparation/FishingNet/Net.cs
+++ C# Advance/Exam Preparation/FishingNet/Net.cs
@@ -1,7 +1,17 @@
-﻿namespace FishingNet
+﻿using System.Collections.Generic;
+
+namespace FishingNet
 {
     public class Net
     {
+        private List<Fish> fish;
+        private string material;
+        private int capacity;
 
+        public Net(string material, int capacity)
+        {
+            this.material = material;
+            this.capacity = capacity;
+        }
     }
 }
