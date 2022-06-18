8a8e2409f1494450d0b319d335c3de52d106918d
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-06-18 14:23:22 +0300

    solved

--- C# Advance/05.Functional Programming/TriFunction/TriFunction.cs
+++ C# Advance/05.Functional Programming/TriFunction/TriFunction.cs
@@ -8,11 +8,38 @@ namespace TriFunction
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            int targetNumber = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(' ').ToList();
+            Func<string, int, bool> func = (name, target) =>
+            {
+                int sum = 0;
+                foreach (var symbol in name)
+                {
+                    sum += symbol;
+                    if (sum >= target)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
 
-            List<string> names = Console.ReadLine().Split().ToList();
-
-            Console.WriteLine(names.First(name => name.Select(symbol => (int) symbol).Sum() >= n));
+            string name = GetName(func, names, targetNumber);
+            Console.WriteLine(name);
         }
+
+        public static string GetName(Func<string, int, bool> func, List<string> names, int targetNumber)
+        {
+            foreach (var name in names)
+            {
+                if (func(name, targetNumber))
+                {
+                    return name;
+                }
+            }
+
+            return null;
+        }
+    
     }
 }
