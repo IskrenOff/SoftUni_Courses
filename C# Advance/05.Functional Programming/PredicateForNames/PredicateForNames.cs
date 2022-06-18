e13c17fb74b913e4467e7559af9e08bc679fddfd
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-06-18 14:18:57 +0300

    solved

---
+++ C# Advance/05.Functional Programming/PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- C# Advance/05.Functional Programming/ReverseAndExclude/ReverseAndExclude.cs
+++ C# Advance/05.Functional Programming/ReverseAndExclude/ReverseAndExclude.cs
@@ -2,20 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ReverseAndExclude
+namespace T06._Reverse_And_Exclude
 {
-    internal class ReverseAndExclude
+    internal class Program
     {
         static void Main(string[] args)
         {
-            List<int> numbers = Console.ReadLine()
-                .Split(' ')
-                .Select(int.Parse)
-                .ToList();
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
 
-            int divisibleNumber = int.Parse(Console.ReadLine());
+            int divider = int.Parse(Console.ReadLine());
 
+            Predicate<int> filter = num => num % divider != 0;
 
+            numbers.Reverse();
+            numbers = MyWhere(numbers, filter);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        static List<int> MyWhere(List<int> numbers, Predicate<int> filter)
+        {
+            List<int> newList = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (filter(number))
+                {
+                    newList.Add(number);
+                }
+            }
+
+            return newList;
         }
     }
 }
