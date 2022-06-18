659a4ce2950c6a27ad85902be1326b7da8dd7c2a
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-06-18 14:03:25 +0300

    solved

---
+++ C# Advance/04.Streams, Files and Directories/LineNumbers/LineNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LineNumbers
+{
+    using System;
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\text.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            ProcessLines(inputPath, outputPath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] allLines = File.ReadAllLines(inputFilePath);
+            List<string> outputLines = new List<string>();
+
+            int rowNumber = 1;
+            foreach (var line in allLines)
+            {
+                int countLetters = line.Count(char.IsLetter);
+                int countNonLetters = line.Count(char.IsPunctuation);
+
+                rowNumber++;
+                string modifiedLine = $"Line {rowNumber}: {line} ({countLetters})({countNonLetters})";
+                outputLines.Add(modifiedLine);
+            }
+            File.WriteAllLines(outputFilePath, outputLines);
+        }
+    }
+}
