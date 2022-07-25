64c44bb0b2ac3ec492cdb293d4ede403b8dd69a3
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-07-25 20:16:05 +0300

    solved

---
+++ C# OOP/Inheritance - Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private int horsePower;
+        private double fuel;
+        private double fuelConsumption;
+        private const double DefaultFuelConsumption = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+
+            FuelConsumption = DefaultFuelConsumption;
+        }
+
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+        public double Fuel
+        {
+            get { return fuel; }
+            set { fuel = value; }
+        }
+        public virtual double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public virtual void Drive (int kilometers)
+        {
+            this.Fuel -= kilometers * FuelConsumption;
+        }
+    }
+}
