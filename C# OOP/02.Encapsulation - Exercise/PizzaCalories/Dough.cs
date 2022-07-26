81e7c814338de8dedb69e40699d56011fd88b548
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-07-26 19:44:37 +0300

    new

---
+++ C# OOP/02.Encapsulation - Exercise/PizzaCalories/Dough.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Dough
+    {
+
+        private const string DoughExceptionMessage = "Invalid type of dough";
+        private const string weightExceptionMessage = "Dough weight should be in the range [1..200].";
+
+        private Dictionary<string, double> flourTypeCalories = new Dictionary<string, double>
+        {
+            { "White" , 1.5 },
+            { "Wholegrain" , 1 },
+        };
+
+        private Dictionary<string, double> bakingTechniqueCalories = new Dictionary<string, double>
+        {
+            { "Crispy", 0.9 },
+            { "Chewy", 1.1 },
+            { "Homemade", 1 },
+        };
+
+        private string flourType;
+        private string bakingTechnique;
+        private double weight;
+
+        public Dough(string flourType, string bakingTechnique, double weight)
+        {
+            FlourType = flourType;
+            BakingTechnique = bakingTechnique;
+            Weight = weight;
+        }
+
+        public string FlourType
+        {
+            get 
+            { 
+                return flourType; 
+            }
+            private set 
+            {
+                if (!bakingTechniqueCalories.ContainsKey(value))
+                {
+                    throw new ArgumentException(DoughExceptionMessage);
+                }
+
+                flourType = value; 
+            }
+        }
+     
+        public string BakingTechnique
+        {
+            get 
+            { 
+                return bakingTechnique; 
+            }
+            private set 
+            {
+                if (!flourTypeCalories.ContainsKey(value))
+                {
+                    throw new ArgumentException(DoughExceptionMessage);
+                }
+
+                bakingTechnique = value;
+            }
+        }
+
+        public double Weight
+        {
+            get 
+            { 
+                return weight; 
+            }
+            private set 
+            {
+                if (value < 1 || value > 200)
+                {
+                    throw new ArgumentException(weightExceptionMessage);
+                }
+
+                weight = value; 
+            }
+        }
+
+        public double Calories
+            => 2 * Weight * flourTypeCalories[this.FlourType] * bakingTechniqueCalories[this.BakingTechnique];
+    }
+}
