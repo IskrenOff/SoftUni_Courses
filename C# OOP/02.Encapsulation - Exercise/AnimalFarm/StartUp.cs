428cd91a84a5ca6cf856d2c9f4909e6ad9e05374
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-07-28 17:35:38 +0300

    solved 100/100

---
+++ C# OOP/02.Encapsulation - Exercise/AnimalFarm/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AnimalFarm.Models;
+
+namespace AnimalFarm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Chicken chicken;
+
+            try
+            {
+                chicken = new Chicken(name, age);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine($"Chicken {chicken.Name} (age {chicken.Age}) can produce {chicken.ProductPerDay} eggs per day.");
+        }
+    }
+}
