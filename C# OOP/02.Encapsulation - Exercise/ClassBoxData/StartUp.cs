463e7f2cab4c55e4363e09f19715f3d320f599f4
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-07-28 17:10:13 +0300

    solved 100/100 Class Box Data

--- C# OOP/02.Encapsulation - Exercise/ClassBoxData/StartUp.cs
+++ C# OOP/02.Encapsulation - Exercise/ClassBoxData/StartUp.cs
@@ -6,7 +6,25 @@ namespace ClassBoxData
     {
         static void Main(string[] args)
         {
-           
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box;
+
+            try
+            {
+                box = new Box(length, width, height);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.Volume():f2}");
         }
     }
 }
