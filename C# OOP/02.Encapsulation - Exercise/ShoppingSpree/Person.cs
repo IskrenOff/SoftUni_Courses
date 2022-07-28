58abe3124419026743d4ec9abee01620740dd3d6
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-07-28 17:59:17 +0300

    solved 100/100

---
+++ C# OOP/02.Encapsulation - Exercise/ShoppingSpree/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private readonly List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            products = new List<Product>();
+        }
+
+        public IReadOnlyCollection<Product> Products => products;
+
+        public string Name
+        {
+            get 
+            { 
+                return name; 
+            }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get 
+            { 
+                return money; 
+            }
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value; 
+            }
+        }
+
+        public void AddProductsToBag (Product product)
+        {
+            if (Money >= product.Cost)
+            {
+                products.Add(product);
+                Money -=product.Cost;
+                Console.WriteLine($"{Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} can't afford {product.Name}");
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Products.Count > 0
+                ? $"{this.Name} - {string.Join(", ", products.Select(p => p.Name))}"
+                : $"{this.Name} - Nothing bought";
+        }
+    }
+}
