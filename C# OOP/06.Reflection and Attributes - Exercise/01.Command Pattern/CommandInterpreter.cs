10ca41ec407404129a894068b4ffce5b8572c19b
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-08-07 13:16:13 +0300

    Unit test exercise

---
+++ C# OOP/06.Reflection and Attributes - Exercise/01.Command Pattern/CommandInterpreter.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Reflection;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern
+{
+    using CommandPattern.Core.Commands;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] inputInfo = args.Split(' ');
+
+            string commandName = inputInfo[0] + "Command";
+            string[] parameters = inputInfo.Skip(1).ToArray();
+
+            Type type = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == commandName);
+
+            ICommand command = Activator.CreateInstance(type) as ICommand;
+            string result = command.Execute(parameters);
+
+            return result;
+        }
+    }
+}
