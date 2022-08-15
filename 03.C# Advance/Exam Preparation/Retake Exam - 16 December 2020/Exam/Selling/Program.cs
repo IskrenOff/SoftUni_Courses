14c33c26d2cc579081d5c106a45df5059d7d2445
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-08-15 20:21:49 +0300

    adding problem to solve

---
+++ 03.C# Advance/Exam Preparation/Retake Exam - 16 December 2020/Exam/Selling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Selling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
