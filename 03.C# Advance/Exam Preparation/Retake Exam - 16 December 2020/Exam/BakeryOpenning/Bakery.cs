e3a79f7334ded927a00fad6de9aa60bc9ca3774e
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-08-16 17:50:03 +0300

    solving old exams

---
+++ 03.C# Advance/Exam Preparation/Retake Exam - 16 December 2020/Exam/BakeryOpenning/Bakery.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> data;
+        private string name;
+        private int capacity;
+
+
+        public Bakery(string name, int capacity)
+        {
+            data = new List<Employee>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                name = value;
+            }
+        }
+        public int Capacity
+        {
+            get => capacity;
+            private set
+            {
+                capacity = value;
+            }
+        }
+
+        public int Count => data.Count;
+
+        public void Add(Employee employee)
+        {
+            if (Count < Capacity)
+            {
+                data.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            return data.Remove(data.Find(x => x.Name == name));
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            int oldestEmployee = data.Select(x => x.Age).Max();
+            return data.Find(e => e.Age == oldestEmployee);
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            return data.Find(x => x.Name == name);
+        }
+
+        public string Report()
+        {
+            StringBuilder output = new StringBuilder();
+
+            output.AppendLine($"Employees working at Bakery {Name}");
+
+            foreach (var employee in data)
+            {
+                output.AppendLine($"{employee}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 03.C# Advance/Exam Preparation/Retake Exam - 16 December 2020/Exam/Openning/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Openning
+{
+    public class Employee
+    {
+        private string name;
+        private int age;
+        private string country;
+
+        public Employee(string name, int age, string capacity)
+        {
+            Name = name;
+            Age = age;
+            Country = capacity;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get => age;
+            private set
+            {
+                age = value;
+            }
+        }
+        public string Country
+        {
+            get => country;
+            private set
+            {
+                country = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Employee: {Name}, {Age}, ({Country})";
+        }
+    }
+}
