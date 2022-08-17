3728cecb50a5de695f350a8602f55f908dcd5ea0
Iskren Mirev <106007460+IskrenOff@users.noreply.github.com>
2022-08-17 19:47:08 +0300

    solved first problem 100/100

---
+++ 03.C# Advance/Exam Preparation/Regular Exam - 25 October 2020/Scheduling/Scheduling.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    internal class Scheduling
+    {
+        static void Main()
+        {
+            string separator = ", ";
+
+            Stack<int> taskValues = new Stack<int>
+                (Console.ReadLine()
+                .Split(separator, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Queue<int> threadValues = new Queue<int>
+                (Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            int target = int.Parse(Console.ReadLine());
+
+
+            while (true)
+            {
+                int task = taskValues.Peek();
+                int thread = threadValues.Peek();
+
+                if (target == task)
+                {
+                    Console.WriteLine($"Thread with value {thread} killed task {target}");
+                    Console.WriteLine(string.Join(' ', threadValues));
+                    break;
+                }
+                else if (task > thread)
+                {
+                    threadValues.Dequeue();
+                }
+                else
+                { 
+                    taskValues.Pop();
+                    threadValues.Dequeue();
+                }
+            }
+        }
+    }
+}
