17c90ccb62d903370183124a2f7e75f0f042e063
IskrenOff <iskren.mirev@gmail.com>
2024-03-29 13:06:31 +0200

    added new class Loan

---
+++ 04.C# OOP/OOPExamsPractice/2023 - 5 August - Regular Exam/BankLoan_Skeleton_netcoreapp6.0/BankLoan/Models/Loan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLoan.Models
+{
+    public abstract class Loan
+    {
+        private int interestRate;
+        private double amount;
+
+        protected Loan(int interestRate, double amount)
+        {
+                
+        }
+    }
+}
