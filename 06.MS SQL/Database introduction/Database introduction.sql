c61e31119ac94b111e11750b6bb577700d9edf8e
IskrenOff <iskren.mirev@gmail.com>
2024-05-16 17:16:08 +0300

    few more Quere

--- 06.MS SQL/Database introduction/Database introduction.sql
+++ 06.MS SQL/Database introduction/Database introduction.sql
@@ -39,3 +39,43 @@ TRUNCATE TABLE Minions
 --06.Drop All Tables
 DROP TABLE Minions
 DROP TABLE Towns
+
+--07.Create Table People
+CREATE TABLE People 
+(
+Id INT IDENTITY(1,1) PRIMARY KEY,
+[Name] NVARCHAR(200) NOT NULL,
+Picture VARBINARY(MAX) NULL,
+Height DECIMAL(5,2) NULL,
+[Weight] DECIMAL(5,2) NULL,
+Gender CHAR(1) NOT NULL CHECK (Gender IN('m','f')),
+Birthdata DATE NOT NULL,
+Biography NVARCHAR(MAX) NULL
+)
+
+INSERT INTO People (Name,Picture,Height,[Weight],Gender,Birthdata,Biography)
+VALUES
+('Iskren Mirev',NULL,1.85,100.32,'m','1984-11-17','John Doe is a fictional character.'),
+('Kalinka Mirev',NULL,1.55,70.12,'m','1986-05-03','John Doe is a fictional character.'),
+('Boby Bol',NULL,1.55,111.42,'m','1981-10-07','John Doe is a fictional character.'),
+('Ivan Leshnika',NULL,1.25,99.12,'m','1988-01-10','John Doe is a fictional character.'),
+('Dimitar Bobara',NULL,1.11,77.72,'m','1991-04-03','John Doe is a fictional character.')
+
+--08.Create Table Users
+CREATE TABLE Users
+(
+Id INT IDENTITY(1,1) PRIMARY KEY,
+Username VARCHAR(30) NOT NULL UNIQUE,
+[Password] VARCHAR(26) NOT NULL UNIQUE,
+ProfilePicture VARBINARY(MAX),
+LastLoginTime DATETIME2,
+IsDeleted BIT
+)
+
+INSERT INTO Users (Username,[Password])
+VALUES
+('Iskrenov','asddadsd'),
+('Kalinchok','asasdasadsd'),
+('Bubka','gergher'),
+('Slona','h45h4h'),
+('Bastun','089fug08f')
