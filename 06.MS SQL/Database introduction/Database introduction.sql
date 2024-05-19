e48418c3157c2fc9a0b6eb1111ed8d619dd544e7
IskrenOff <iskren.mirev@gmail.com>
2024-05-19 23:08:26 +0300

    new inserts

--- 06.MS SQL/Database introduction/Database introduction.sql
+++ 06.MS SQL/Database introduction/Database introduction.sql
@@ -271,6 +271,205 @@ INSERT INTO RentalOrders (EmployeeId, CustomerId, CarId, KilometrageStart) VALUE
 	(2, 3, 2, 2000),
 	(3, 1, 1, 20000)
 
+--15. Hotel Database
+CREATE TABLE Employees(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+	FirstName NVARCHAR(50) NOT NULL,
+	LastName NVARCHAR(50) NOT NULL,
+	Title NVARCHAR(50),
+	Notes NVARCHAR(MAX)
+)
+
+INSERT INTO Employees(FirstName, LastName) VALUES
+    ('Ivan', 'Ivanov'),
+	('Georgi', 'Don'),
+	('Toni', 'Ton')
+
+--SELECT * FROM Employees
+
+CREATE TABLE Customers(
+    AccountNumber INT PRIMARY KEY NOT NULL,
+	FirstName NVARCHAR(50) NOT NULL,
+	LastName NVARCHAR(50) NOT NULL,
+	PhoneNumber VARCHAR(10) NOT NULL,
+	EmergencyName NVARCHAR(50),
+	EmergencyNumber VARCHAR(12),
+	Notes NVARCHAR(MAX)
+)
+
+INSERT INTO Customers(AccountNumber, FirstName, LastName, PhoneNumber) VALUES
+    (55, 'Pesho', 'Ivanov', '0959759957'), 
+	(44, 'Gosho', 'Sasg', '1232435354'),
+	(33, 'Alek', 'Ivanov', '0954345427')
+
+--SELECT * FROM Customers
+
+CREATE TABLE RoomStatus(
+    RoomStatus NVARCHAR(50) PRIMARY KEY NOT NULL,
+	Notes NVARCHAR(MAX)
+)
+
+INSERT INTO RoomStatus(RoomStatus) VALUES
+    ('Full'),
+	('Empty'),
+	('Half')
+
+--SELECT * FROM RoomStatus
+
+CREATE TABLE RoomTypes(
+    RoomType NVARCHAR(50) PRIMARY KEY NOT NULL,
+	Notes NVARCHAR(MAX)
+)
+
+INSERT INTO RoomTypes(RoomType) VALUES
+    ('Large'),
+	('Medium'),
+	('Small')
+
+--SELECT * FROM RoomTypes
+
+CREATE TABLE BedTypes(
+    BedType NVARCHAR(50) PRIMARY KEY NOT NULL,
+	Notes NVARCHAR(MAX)
+)
+
+INSERT INTO BedTypes(BedType) VALUES
+    ('Double'),
+	('Single'),
+	('Family')
+
+--SELECT * FROM BedTypes
+
+CREATE TABLE Rooms(
+    RoomNumber INT PRIMARY KEY NOT NULL,
+	RoomType NVARCHAR(50) FOREIGN KEY REFERENCES RoomTypes(RoomType) NOT NULL,
+	BedType NVARCHAR(50) FOREIGN KEY REFERENCES BedTypes(BedType) NOT NULL,
+	Rate INT,
+	RoomStatus NVARCHAR(50) FOREIGN KEY REFERENCES RoomStatus(RoomStatus) NOT NULL,
+	Notes NVARCHAR(MAX)
+)
+
+INSERT INTO Rooms(RoomNumber, RoomType, BedType, RoomStatus) VALUES
+    (1, 'Large', 'Double', 'Full'),
+	(2, 'Medium', 'Single', 'Half'),
+	(3, 'Small', 'Family', 'Empty')
+
+--SELECT * FROM Rooms
+
+CREATE TABLE Payments(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+	EmployeeId INT FOREIGN KEY REFERENCES Employees(Id) NOT NULL,
+	PaymentDate DATE,
+	AccountNumber INT,
+	FirstDateOccupied DATE,
+	LastDateOccupied DATE,
+	TotalDays INT,
+	AmountCharged DECIMAL(15,2),
+	TaxRate INT,
+	TaxAmount DECIMAL(15,2),
+	PaymentTotal DECIMAL(15,2),
+	Notes NVARCHAR(MAX)
+)
+
+INSERT INTO Payments(EmployeeId) VALUES
+    (1),
+	(2),
+	(3)
+
+--SELECT * FROM Payments
+
+CREATE TABLE Occupancies(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+	EmployeeId INT FOREIGN KEY REFERENCES Employees(Id) NOT NULL,
+	DateOccupied DATE,
+	AccountNumber INT FOREIGN KEY REFERENCES Customers(AccountNumber) NOT NULL,
+	RoomNumber INT FOREIGN KEY REFERENCES Rooms(RoomNumber) NOT NULL,
+	RateApplied INT,
+	PhoneCharge DECIMAL(15,2),
+	Notes NVARCHAR(MAX)
+)
+
+INSERT INTO Occupancies (EmployeeId, AccountNumber, RoomNumber) VALUES
+    (1, 55, 1),
+	(2, 44, 2),
+	(3, 33, 3)
+
+--Basic Insert
+CREATE DATABASE SoftUni
+
+CREATE TABLE Towns(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Addresses(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+	AddressText NVARCHAR(100) NOT NULL,
+	TownId INT FOREIGN KEY REFERENCES Towns(Id) NOT NULL
+)
+
+CREATE TABLE Departments(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Employees(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+	FirstName NVARCHAR(50) NOT NULL,
+	MiddleName NVARCHAR(50),
+	LastName NVARCHAR(50) NOT NULL,
+	JobTitle NVARCHAR(50) NOT NULL,
+	DepartmentId INT FOREIGN KEY REFERENCES Departments(Id) NOT NULL,
+	HireDate DATE,
+	Salary DECIMAL(15,2) NOT NULL,
+	AddressId INT FOREIGN KEY REFERENCES Addresses(Id)
+)
+
+
+INSERT INTO Towns ([Name]) VALUES
+    ('Sofia'),
+	('Plovdiv'),
+	('Varna'),
+	('Burgas')
+
+INSERT INTO Departments ([Name]) VALUES
+    ('Engineering'),
+	('Sales'),
+	('Marketing'),
+	('Software Development'),
+	('Quality Assurance')
+
+INSERT INTO Employees (FirstName, MiddleName, LastName, JobTitle, DepartmentId, HireDate, Salary) VALUES
+    ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer',	4,	CONVERT(datetime, '01/02/2013', 103), 3500.00),
+	('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1,	CONVERT(datetime, '02/03/2004', 103), 4000.00),
+	('Maria', 'Petrova', 'Ivanova', 'Intern',	5,	CONVERT(datetime, '28/08/2016', 103), 525.25),
+	('Georgi', 'Teziev', 'Ivanov', 'CEO',	2,	CONVERT(datetime, '09/12/2007', 103), 3000.00),
+	('Peter', 'Pan', 'Pan', 'Intern',	3,	CONVERT(datetime, '28/08/2016', 103), 599.88)
+
+--Exercise 20
+SELECT * FROM Towns
+ORDER BY [Name]
+SELECT * FROM Departments
+ORDER BY [Name]
+SELECT * FROM Employees
+ORDER BY Salary DESC
+
+--Exercise 21
+
+SELECT [Name] FROM Towns
+ORDER BY [Name]
+SELECT [Name] FROM Departments
+ORDER BY [Name]
+SELECT FirstName, LastName, JobTitle, Salary FROM Employees
+ORDER BY SALARY DESC
+
+--Exercise 22
+
+UPDATE Employees
+SET Salary+=Salary*0.1
+
+SELECT Salary FROM Employees
+
 
 
 
