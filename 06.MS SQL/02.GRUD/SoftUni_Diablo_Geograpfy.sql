d863b578b7076080f98fdc19fc63e26fc55923d2
IskrenOff <iskren.mirev@gmail.com>
2024-05-20 10:32:47 +0300

    adding some new req

--- 06.MS SQL/02.GRUD/SoftUni_Diablo_Geograpfy.sql
+++ 06.MS SQL/02.GRUD/SoftUni_Diablo_Geograpfy.sql
@@ -18,3 +18,23 @@ SELECT FirstName + '.' + LastName + '@softuni.bg'
 
 --7. Find All Different Employees' Salaries
 SELECT DISTINCT Salary FROM Employees
+
+--8. Find All Information About Employees
+SELECT * FROM Employees
+    WHERE JobTitle = 'Sales Representative'
+
+--9. Find Names of All Employees by Salary in Range
+SELECT FirstName, LastName, JobTitle 
+  FROM Employees
+ WHERE Salary BETWEEN 20000 AND 30000
+
+--10. Find Names of All Employees
+SELECT CONCAT(FirstName, ' ',  MiddleName + ' ', LastName) 
+    AS [Full Name]
+  FROM Employees
+ WHERE Salary IN (25000, 14000, 12500, 23600)
+
+--11. Find All Employees Without a Manager
+SELECT FirstName, LastName 
+  FROM Employees
+ WHERE ManagerID IS NULL
