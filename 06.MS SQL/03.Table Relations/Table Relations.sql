979d86c60dafaac2eb447e63210c8a536ef25d47
IskrenOff <iskren.mirev@gmail.com>
2024-05-21 12:15:17 +0300

    adding new

--- 06.MS SQL/03.Table Relations/Table Relations.sql
+++ 06.MS SQL/03.Table Relations/Table Relations.sql
@@ -26,3 +26,101 @@ INSERT INTO Persons (FirstName, Salary, PassportID) VALUES
 
 SELECT * FROM Persons
 SELECT * FROM Passports
+
+-- 02. One-To-Many Relationship
+CREATE TABLE Models(
+	ModelID INT NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
+	ManufacturerID INT NOT NULL
+)
+
+CREATE TABLE Manufacturers(
+	ManufacturerID INT NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
+	EstablishedOn DATE NOT NULL
+)
+
+INSERT INTO Models(ModelID, [Name], ManufacturerID) VALUES
+	(101, 'X1', 1),
+	(102, 'i6', 1),
+	(103, 'Model S', 2),
+	(104, 'Model X', 2),
+	(105, 'Model 3', 2),
+	(106, 'Nova', 3)
+
+INSERT INTO Manufacturers(ManufacturerID, [Name], EstablishedOn) VALUES
+	(1, 'BMW', '07/03/1916'),
+	(2, 'Tesla', '01/01/2003'),
+	(3, 'Lada', '01/05/1966')
+
+ALTER TABLE Models
+ADD CONSTRAINT PK_Models
+PRIMARY KEY (ModelID)
+
+ALTER TABLE Manufacturers
+ADD CONSTRAINT PK_Manufacturers
+PRIMARY KEY (ManufacturerID)
+
+ALTER TABLE Models
+ADD CONSTRAINT FK_Models_Manufacturers
+FOREIGN KEY (ManufacturerID)
+REFERENCES Manufacturers(ManufacturerID)
+
+SELECT * FROM Models
+SELECT * FROM Manufacturers
+
+-- 03. Many-To-Many Relationship 
+CREATE TABLE Students(
+	StudentID INT NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Exams(
+	ExamID INT NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE StudentsExams(
+	StudentID INT NOT NULL,
+	ExamID INT NOT NULL
+)
+
+INSERT INTO Students (StudentID, [Name]) VALUES
+	(1, 'Mila'),
+	(2, 'Toni'),
+	(3, 'Roni')
+
+INSERT INTO Exams (ExamID, [Name]) VALUES
+	(101, 'SpringMVC'),
+	(102, 'Neo4j'),
+	(103, 'Oracle 11g')
+
+INSERT INTO StudentsExams (StudentID, ExamID) VALUES
+	(1, 101),
+	(1, 102),
+	(2, 101),
+	(3, 103),
+	(2, 102),
+	(2, 103)
+
+ALTER TABLE Students
+ADD CONSTRAINT PK_Students
+PRIMARY KEY (StudentID)
+
+ALTER TABLE Exams
+ADD CONSTRAINT PK_Exams
+PRIMARY KEY(ExamID)
+
+ALTER TABLE StudentsExams
+ADD CONSTRAINT FK_StudentsExams_Students
+FOREIGN KEY (StudentID)
+REFERENCES Students(StudentID)
+
+ALTER TABLE StudentsExams
+ADD CONSTRAINT FK_StudentsExams_Exams
+FOREIGN KEY (ExamID)
+REFERENCES Exams(ExamID)
+
+ALTER TABLE StudentsExams
+ADD CONSTRAINT PK_StudentsExams
+PRIMARY KEY(StudentID, ExamID)
