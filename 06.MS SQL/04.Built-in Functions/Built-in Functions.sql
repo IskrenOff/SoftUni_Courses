3bdca6a3145c7b4962e14953a300d04dbcc15e86
IskrenOff <iskren.mirev@gmail.com>
2024-05-23 13:02:24 +0300

    adding 18 19

--- 06.MS SQL/04.Built-in Functions/Built-in Functions.sql
+++ 06.MS SQL/04.Built-in Functions/Built-in Functions.sql
@@ -120,3 +120,23 @@ SELECT ProductName, OrderDate,
        DATEADD(DAY, 3, OrderDate) AS [Pay Due],
 	   DATEADD(MONTH, 1, OrderDate) AS [Delivery Due]
   FROM Orders
+
+-- 19. People Table
+CREATE TABLE People(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+	[Name] NVARCHAR(100),
+	Birthdate DATETIME
+)
+
+INSERT INTO People ([Name], Birthdate) 
+VALUES ('Victor', '2000-12-07' ),
+       ('Steven', '1992-09-10'),
+	   ('Stephen', '1910-09-19 '),
+	   ('John', '2010-01-06 ')
+
+SELECT [Name], 
+       DATEDIFF(YEAR, Birthdate, GETDATE()) AS [Age in Years],
+	   DATEDIFF(MONTH, Birthdate, GETDATE()) AS [Age in Months],
+	   DATEDIFF(DAY, Birthdate, GETDATE()) AS [Age in Days],
+	   DATEDIFF(MINUTE, Birthdate, GETDATE()) AS [Age in Minutess]
+  FROM People
